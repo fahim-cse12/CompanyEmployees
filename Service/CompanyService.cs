b843881ad86b610b4d457cdfa0900309cb3846bc
BRACITS\forhad.hossain <forhad.hossain@bracits.com>
2024-02-01 15:27:51 +0600

    Implement Automapper

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,11 +1,7 @@
-﻿using Contracts;
-using Entities.Models;
+﻿using AutoMapper;
+using Contracts;
 using Service.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Shared.DataTransferObjects;
 
 namespace Service
 {
@@ -13,24 +9,29 @@ namespace Service
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
-        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+        private readonly IMapper _mapper;
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;   
         }
 
-        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
             try
             {
                 var companies = _repository.Company.GetAllCompanies(trackChanges);
-                return companies;
+                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+                return companiesDto;
+
             }
             catch (Exception ex)
             {
-                _logger.LogError($"Something went wrong in the{ nameof(GetAllCompanies)} service method { ex} ");
+                _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)}service method { ex}");
                 throw;
             }
+
         }
     }
 }
